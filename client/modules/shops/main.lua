e75caa38e2ba62920d43000d3f48fdb0317ad40d
Mirow <maxim.doerr@icloud.com>
2025-02-09 10:27:37 +0100

    feat(shops): added target option

--- client/modules/shops/main.lua
+++ client/modules/shops/main.lua
@@ -1,4 +1,11 @@
-local shops, n = {}, 0
+local shops = {}
+
+local function openShop(shopKey)
+    local shop = shops[shopKey]
+    if not shop then return end
+
+    Shared.debug(('Opening shop \'%s\'!'):format(shopKey))
+end
 
 for key, shopData in pairs(lib.load('data.shops.shops')) do
     local blip = shopData.blip
@@ -7,7 +14,7 @@ for key, shopData in pairs(lib.load('data.shops.shops')) do
     end
 
     local cashier = shopData.cashier
-    lib.points.new({
+    local point = lib.points.new({
         shopKey = key,
         coords = cashier.coords.xyz,
         distance = 50,
@@ -16,15 +23,29 @@ for key, shopData in pairs(lib.load('data.shops.shops')) do
 
             local entity = Client.spawnPed(cashier.model, cashier.coords, cashier.animation)
             if not entity then return Shared.debug(('Could not spawn npc \'%s\'!'):format(key)) end
-            --exports.ox_target:addLocalEntity(entity, mappedOptions)
+            exports.ox_target:addLocalEntity(entity, {
+                {
+                    label = 'Einkaufen',
+                    icon = 'fas fa-shopping-cart',
+                    onSelect = function()
+                        openShop(key)
+                    end
+                }
+            })
         end,
         onExit = function(self)
             local entity = self.entity
             if not entity then return end
 
-            --exports.ox_target:removeLocalEntity(entity)
+            exports.ox_target:removeLocalEntity(entity)
             Client.deleteEntity(entity)
             self.entity = nil
         end
     })
+
+    shops[key] = {
+        key = key,
+        point = point,
+        data = shopData
+    }
 end
