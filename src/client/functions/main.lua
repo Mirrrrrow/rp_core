22df50c2aad484f76c3d9b8168a643bff20739d1
Mirow <maxim.doerr@icloud.com>
2025-02-21 19:09:55 +0100

    feat: register functions

--- src/client/functions/main.lua
+++ src/client/functions/main.lua
@@ -5,6 +5,8 @@ return {
     spawnPed = require(FUNCTIONS_PATH:format('spawnPed')),
     ---@type fun(data: PedInteractionData)
     addPedInteraction = require(FUNCTIONS_PATH:format('addPedInteraction')),
-    ---@type fun(model: string|number): number?
+    ---@type fun(model: string|number): VehicleData
     parseVehicleData = require(FUNCTIONS_PATH:format('parseVehicleData')),
+    ---@type fun(data: PaymentMethodData): string|false
+    selectPaymentMethod = require(FUNCTIONS_PATH:format('selectPaymentMethod')),
 }
