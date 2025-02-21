4bdae5ebc5d0fb1780410d68c2ab45fc5d1e547b
Mirow <maxim.doerr@icloud.com>
2025-02-21 17:07:16 +0100

    feat: addPedInteraction function to create simple interactions

---
+++ src/client/functions/addPedInteraction.lua
@@ -0,0 +1,40 @@
+---@class PedInteractionData
+---@field key? string
+---@field ped PedData
+---@field interactions PedInteraction|PedInteraction[]
+
+---@class PedInteraction
+---@field label string
+---@field icon string
+---@field onSelect function
+
+---@param data PedInteractionData
+local function addPedInteraction(data)
+    local ped = data.ped
+    lib.points.new({
+        coords = ped.coords.xyz,
+        distance = 50,
+        onEnter = function(self)
+            if self.entity then return end
+
+            local entity = Client.functions.spawnPed({
+                coords = ped.coords,
+                model = ped.model,
+                animation = ped.animation,
+            })
+
+            if not entity then return Shared.debug(('Could not spawn npc \'%s\'!'):format(data.key or 'PedInteraction')) end
+            exports.ox_target:addLocalEntity(entity, data.interactions)
+        end,
+        onExit = function(self)
+            local entity = self.entity
+            if not entity then return end
+
+            exports.ox_target:removeLocalEntity(entity)
+            Client.deleteEntity(entity)
+            self.entity = nil
+        end
+    })
+end
+
+return addPedInteraction
--- src/client/functions/main.lua
+++ src/client/functions/main.lua
@@ -3,4 +3,6 @@ return {
     createBlip = require(FUNCTIONS_PATH:format('createBlip')),
     ---@type fun(data: PedData): number?
     spawnPed = require(FUNCTIONS_PATH:format('spawnPed')),
+    ---@type fun(data: PedInteractionData)
+    addPedInteraction = require(FUNCTIONS_PATH:format('addPedInteraction')),
 }
