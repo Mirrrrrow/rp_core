e7c954f3ea6ace7581e51622089b828e4975da8c
Mirow <maxim.doerr@icloud.com>
2025-02-22 17:24:52 +0100

    feat(devTools): add tools

---
+++ src/client/modules/devTools/main.lua
@@ -0,0 +1,30 @@
+RegisterCommand('pvd', function(source, args, raw)
+    if not cache.vehicle then
+        return lib.notify({ type = 'error', description = 'You are not in a vehicle!' })
+    end
+    lib.print.info(lib.getVehicleProperties(cache.vehicle))
+    lib.print.info(Entity(cache.vehicle).state.vehicleId)
+end)
+
+CreateThread(function()
+    exports.ox_target:addGlobalObject({
+        {
+            label = 'Copy model',
+            icon = 'fas fa-cube',
+            onSelect = function(data)
+                ---@diagnostic disable-next-line: param-type-mismatch
+                lib.setClipboard(GetEntityModel(data.entity))
+                lib.notify({ type = 'success', description = 'Model copied!' })
+            end
+        },
+        {
+            label = 'Copy coords',
+            icon = 'fas fa-map-marker-alt',
+            onSelect = function(data)
+                local coords = GetEntityCoords(data.entity)
+                lib.setClipboard(coords.x .. ', ' .. coords.y .. ', ' .. coords.z)
+                lib.notify({ type = 'success', description = 'Coords copied!' })
+            end
+        }
+    })
+end)
