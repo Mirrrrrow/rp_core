cbca49b75f1afae9d4448bf5e3785207693fdd8f
Mirow <maxim.doerr@icloud.com>
2025-02-27 17:23:53 +0100

    chore(enums): add note

--- data/enums.lua
+++ data/enums.lua
@@ -1,3 +1,9 @@
+--[[
+    This file contains all the enums used in the server.
+    You dont need to touch this file.
+    And you should not touch this file...
+]]
+
 PARK_STATES = {
     NONE = 0,
     GARAGE = 1,
